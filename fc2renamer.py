8e48b824c6fe330122d70769ba267d0860ddf774
Tony Lin <tonyatta.lin@gmail.com>
2021-01-04 11:56:25 +0800

    only create chrome driver once, do not load image from the website.

--- fc2renamer.py
+++ fc2renamer.py
@@ -9,10 +9,12 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 logging.basicConfig(stream=sys.stdout)
 logger = logging.getLogger("fc2rename")
-
+chrome = None
 
 def remove_punctuation_map(_unistr):
     remove_punctuation_map = dict((ord(char), None) for char in '\/*?:"<>|')
@@ -28,26 +30,42 @@ def str2code(filename):
             return m.group(1)
 
 
-def code2filename(code):
-    options = Options()
-    options.add_argument("--disable-notifications")
-    options.headless = True
+def code2filename(code, browser=False):
+    global chrome
+    if not chrome:
+        options = Options()
+        options.add_argument("--disable-notifications")
+        if not browser:
+            options.headless = True
 
-    chromedriver = os.path.join(os.path.dirname(os.path.realpath(__file__)), "chromedriver")
-    chrome = webdriver.Chrome(chromedriver, options=options)
+        chromedriver = os.path.join(os.path.dirname(os.path.realpath(__file__)), "chromedriver")
+        # options.add_argument("user-data-dir={}".format("chrome-fc2renamer"))
+        options.add_argument("--disable-notifications") 
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        options.add_experimental_option("prefs", prefs)
+
+        chrome = webdriver.Chrome(chromedriver, options=options)
+        # chrome.set_page_load_timeout(5)
+        # chrome.implicitly_wait(10) 
+
     url = "https://adult.contents.fc2.com/article/{}/".format(code)
     chrome.get(url)
-    seller = chrome.find_elements(By.XPATH, '//div[@class="items_article_headerInfo"]//a[contains(@href, "https://adult.contents.fc2.com/users/")]')
+
+    # wait = WebDriverWait(chrome, 10)
+    # wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="items_article_headerInfo"]')))
+
     title = chrome.find_elements(By.XPATH, '//div[@class="items_article_headerInfo"]/h3')
+    seller = chrome.find_elements(By.XPATH, '//div[@class="items_article_headerInfo"]//a[contains(@href, "https://adult.contents.fc2.com/users/")]')
 
     if title and seller:
-        seller_str = seller[0].text
+        seller_str = remove_punctuation_map(seller[0].text)[:80]
         title_str = remove_punctuation_map(title[0].text)[:80]
         return os.path.join(seller_str, "FC2-{}-{}".format(code, title_str))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='filename')
     parser.add_argument('-v', dest='verbose', help='verbose', action='store_true')
+    parser.add_argument('-b', dest='browser', action='store_true')
 
     args, unparsed = parser.parse_known_args()
     if args.verbose:
@@ -61,21 +79,18 @@ if __name__ == '__main__':
         if not os.path.isfile(filename):
             continue
 
-        # parse folder namek
+        # parse folder name
         dirname = os.path.dirname(filename)
         basename = os.path.basename(filename)
         code = str2code(basename)
         if code in seen:
             folder = seen[code]
         else:
-            folder = code2filename(code)
+            folder = code2filename(code, args.browser)
             seen[code] = folder
         logger.debug("title of %s = \"%s\"" % (basename, folder))
 
         if folder:
-            # remove ! from title
-            # folder = "FC2-{}-{}".format(code, title[:80])
-            # folder = os.path.join(dirname, folder)
             try:
                 if not os.path.exists(folder):
                     os.makedirs(folder)
@@ -89,4 +104,4 @@ if __name__ == '__main__':
                     logger.error("%s exist" % (new_file))
             except OSError as e:
                 logger.error(e)
-
+    chrome.quit()
